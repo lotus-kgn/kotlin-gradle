ae827b0b76afdfc8b73bee1da0fe43ea5f993e6c
mspas <ms.paschenko28@gmail.com>
2024-09-08 16:18:06 +0300

    wad

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package com.lotus
 
+import com.sun.java.swing.plaf.windows.resources.windows_de
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
@@ -13,4 +15,3 @@ fun main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
     }
-}
